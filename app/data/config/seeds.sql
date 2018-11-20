cb0c8db4dd5fabe871bc5aa630c8a809f95b7a26
Justin Kunz <Justin_Kunz@yahoo.com>
2018-11-19 20:13:10 -0600

    Corrected seeds file

--- app/data/config/seeds.sql
+++ app/data/config/seeds.sql
@@ -1,6 +1,6 @@
-use friend_finder_db2;
+use friend_finder_db;
 
-insert into user_info2(user_data)
+insert into user_info(user_data)
 values 
 ('{"img": "https://media1.popsugar-assets.com/files/thumbor/eOF2Umn-mqNGnohxrtjeurwWDmI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/08/20/677/n/1922283/1118a12c5b7adb1e342de9.55515725_/i/Michael-Scott-Misquotations-Office-Video.jpg", "name": "Michael Scott", "answers": ["5", "2", "3", "1", "5", "3", "2", "4", "5", "3"]}'),
 ('{"img": "https://pbs.twimg.com/profile_images/549268771484229632/WnatiHzT_400x400.jpeg", "name": "Dwight K. Schrute", "answers": ["3", "5", "5", "5", "1", "5", "5", "4", "4", "5"]}'),
