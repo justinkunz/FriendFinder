8c390a425bea7b27d9b3b0388d1d961d7322d89f
Justin Kunz <Justin_Kunz@yahoo.com>
2018-11-19 19:41:20 -0600

    added JAWS DB code for heroku deployment

--- app/data/connection.js
+++ app/data/connection.js
@@ -1,14 +1,18 @@
 module.exports = function () {
- 
+
     var sql = require('mysql')
-    conn = sql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'password',
-        database: 'friend_finder_db'
-    })
-    
+    if (process.env.JAWSDB_URL) {
+        conn = sql.createConnection(process.env.JAWSDB_URL)
+    }
+    else {
+        conn = sql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: 'password',
+            database: 'friend_finder_db'
+        })
+    }
     conn.connect(function (e) {
         if (e) {
             console.log('error: ' + e.stack)
